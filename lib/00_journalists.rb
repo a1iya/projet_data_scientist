4f14d49a351f1a1e0157502b26fa3fb562e0d983
Aliya Gizatullina <aliyusha.gizatullina@gmail.com>
2019-01-12 14:37:44 +0100

    Exercices terminés du vendredi

---
+++ lib/00_journalists.rb
@@ -0,0 +1,121 @@
+$handles = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+# on va déclarer toutes nos fonctions ici
+def Question1
+    answer = "Il y a %d journalistes dans cet array." % [$handles.count]
+end
+
+def Question2
+  i = 0
+  nbWithDigit = 0
+  while(i < $handles.count)
+    hasNum = $handles[i] =~ /\d/
+    if(hasNum)
+      nbWithDigit += 1
+    end
+    i += 1
+  end
+
+  answer = "Au doigt mouillé, je dirais, environ, à la louche %d journalistes, avec un numéro" % [nbWithDigit]
+end
+
+def Question3
+  i = 0
+  nbWithAUDE = 0
+  while(i < $handles.count)
+    hasNum = $handles[i] =~ /aude/i
+    if(hasNum)
+      nbWithAUDE += 1
+    end
+    i += 1
+  end
+    answer = "Désolé, j'étais parti boire un café. Tu disais ? ah oui, avec -aude-, j'en ai trouvé %d. Si tu me crois pas, tu recomptes." % [nbWithAUDE]
+end
+
+def Question4
+  i=0
+  nbWithMAJ = 0
+  while( i< $handles.count)
+    isMAJ = $handles[i][1] =~ /[A-Z]/
+    if(isMAJ)
+      nbWithMAJ += 1
+    end
+    i += 1
+  end
+  answer = "Ils sont %d journalistes à avoir utilisé une majuscule pour leur première lettre" % [nbWithMAJ]
+end
+
+def Question5
+  i=0
+  nbWithMAJ = 0
+  while( i< $handles.count)
+    hasMAJ = $handles[i] =~ /[A-Z]/
+    if(hasMAJ)
+      nbWithMAJ += 1
+    end
+    i += 1
+  end
+  answer = "Ils sont %d journalistes à avoir utilisé une majuscule" % [nbWithMAJ]
+end
+
+def Question6
+  i=0
+  nbUnderscore = 0
+  while( i< $handles.count)
+    nbUnderscore += $handles[i].count('_')
+    i += 1
+  end
+  answer = "Je dénombre exactement %d underscore (tu sais, le tiret du bas, sur la touche 8, sur PC... pas sur MAC, je sais jamais où elle est cette touche)" % [nbUnderscore]
+end
+
+def Question7
+  h2 = $handles.sort
+  puts h2
+  answer = "et voilà c'est trié"
+end
+
+def Question8
+  h2 = $handles.sort_by {|x| x.length}
+  puts h2[0...50]
+  answer "tu m'as bien cassé le cerveau. J'ai mérité une sieste."
+end
+
+def Question9
+  handle = '@epenser'
+  idx = $handles.index(handle)
+  answer = "Ce cher %s est en %dè position" % [handle, idx+1]
+end
+
+#main
+puts "Bonjour l'ami !"
+moreAnswer = true
+while moreAnswer == true
+  system "clear"
+  puts "A quelle question désirez-vous obtenir une réponse ?"
+  puts " 1 - Combien y a-t-il de journalistes dans ce array ?"
+  puts " 2 - Combien d'handle contiennent un numéro ?"
+  puts " 3 - Combien d'handle contiennent les 4 lettres du prénom \"Aude\" à la suite (sans prendre en compte les majuscules) ?"
+  puts " 4 - Combien commencent par une majuscule (première lettre juste après le @) ?"
+  puts " 5 - Combien contiennent une majuscule ?"
+  puts " 6 - Combien y a-t-il de underscore _ dans tous les pseudos confondus ?"
+  puts " 7 - Trie la liste de handle par ordre alphabétique."
+  puts " 8 - Quels sont les 50 handles les plus courts de ce array ?"
+  puts " 9 - Quelle est la position dans l'array de la personne @epenser ?"
+  print "> "
+  choice = gets.chomp.to_i
+
+  begin
+    answer = eval "Question"+choice.to_s+"()"
+    puts "\n"+answer
+
+    puts "\nDésirez-vous que je réponde à une autre question ? (O/N)"
+    print "> "
+    choice = gets.chomp.to_s
+    if choice != "o" && choice != "O"
+      moreAnswer = false
+      puts "A bientôt !"
+    end
+    rescue 
+      puts "j'ai rien compris... on va dire que la réponse est 42."
+  end
+end
